5b38e18256411b73ea61606da079fc068e87d131
jackiee211 <nggaman111@gmail.com>
2025-02-13 00:05:34 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-<<<<<<< HEAD
-  base: '/ecommerce-react/', // Make sure this matches your GitHub Pages URL
-=======
-  base: '/ecommerce-react/',
->>>>>>> 3266266 (deployment issue)
+  base: process.env.NODE_ENV === 'production' ? '/ecommerce-react/' : '/',
   plugins: [react()],
 });

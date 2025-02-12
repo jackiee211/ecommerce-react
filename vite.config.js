bb62f46d8b3960e1cc3f171e117e31ed9919792c
jackiee211 <163046737+jackiee211@users.noreply.github.com>
2025-02-12 23:57:53 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/ecommerce-react/', // Make sure this matches your GitHub Pages URL
   plugins: [react()],
-})
+});
